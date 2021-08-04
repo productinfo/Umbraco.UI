0b5a347b75f8b86fa81a736a230cffac65534db0
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2021-08-04 14:54:05 +0200

    Added test for show/hide toggle

--- src/components/uui-password-input/uui-password-input.test.ts
+++ src/components/uui-password-input/uui-password-input.test.ts
@@ -1,18 +1,27 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import { html, fixture, expect, elementUpdated } from '@open-wc/testing';
 import '.';
 import { UUIPasswordInput } from './uui-password-input.element';
 
 describe('UuiPasswordInput', () => {
   let element: UUIPasswordInput;
   beforeEach(async () => {
-    element = await fixture(html`<uui-password-input></uui-password-input>`);
+    element = await fixture(
+      html`<uui-password-input
+        .toggleText=${{ showText: 'Show', hideText: 'Hide' }}
+      ></uui-password-input>`
+    );
   });
 
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
   });
 
-  it('test that disable works', async () => {
-    return false;
+  it('Show/Hide toggle', async () => {
+    const toggle = element.shadowRoot?.querySelector('.password-toggle');
+
+    expect(toggle?.textContent?.trim()).to.eq('Show');
+    (toggle as HTMLElement).click();
+    await elementUpdated(element);
+    expect(toggle?.textContent?.trim()).to.eq('Hide');
   });
 });
