e5174e1673ccac8844450b84eb10c470f9d4768a
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2021-08-11 11:25:08 +0200

    cleanup

--- src/components/uui-overlay/uui-overlay.element.ts
+++ src/components/uui-overlay/uui-overlay.element.ts
@@ -54,7 +54,6 @@ export class UUIOverlayElement extends LitElement {
   @query('#container') containerElement?: HTMLElement;
 
   @property({ type: Number }) margin = 0;
-  @property({ type: Boolean }) useAutoPlacement = false;
   @property({ type: Object }) parent?: Element;
   @property({ type: String })
   get overlayPos() {
@@ -65,7 +64,6 @@ export class UUIOverlayElement extends LitElement {
     this.overlayPosCurrent = newValue;
     this.updateOverlay();
   }
-
   @property({ type: Boolean })
   get open() {
     return this._open;
@@ -79,15 +77,6 @@ export class UUIOverlayElement extends LitElement {
     this.rootElement = this.shadowRoot?.host as HTMLElement;
   }
 
-  // connectedCallback() {
-  //   super.connectedCallback();
-  // // document.addEventListener('scroll', () => this.updateOverlay());
-  // }
-  // disconnectedCallback() {
-  //   // document.removeEventListener('scroll', () => this.updateOverlay());
-  //   super.disconnectedCallback();
-  // }
-
   initOverlay() {
     if (this.rootElement) {
       //Reset Styling
@@ -97,13 +86,11 @@ export class UUIOverlayElement extends LitElement {
         this.updateOverlay();
         this.createOberserver();
         this.rootElement!.style.opacity = '1';
-        // document.addEventListener('scroll', () => this.updateOverlay());
       }, 0);
     }
   }
 
   closeOverlay() {
-    // document.removeEventListener('scroll', () => this.updateOverlay());
     this.intersectionObserver?.disconnect();
   }
 
@@ -122,19 +109,16 @@ export class UUIOverlayElement extends LitElement {
     this.intersectionObserver.observe(this.containerElement as Element);
   }
 
-  intersectionCallback = (
-    entries: IntersectionObserverEntry[],
-    observer: any
-  ) => {
+  intersectionCallback = (entries: IntersectionObserverEntry[]) => {
     entries.forEach(element => {
       if (!element.isIntersecting) {
-        this.SetFlipSide(element.intersectionRect);
+        this.setFlipSide(element.intersectionRect);
         this.updateOverlay();
       }
     });
   };
 
-  SetFlipSide(rect: DOMRectReadOnly) {
+  setFlipSide(rect: DOMRectReadOnly) {
     const sideSplit = this.overlayPosCurrent.split(/(?=[A-Z])/);
     const currentSide = sideSplit[0];
     const sideSuffix = sideSplit[1] || 'Center';
@@ -182,13 +166,207 @@ export class UUIOverlayElement extends LitElement {
     const rootElement = this.rootElement!;
 
     this.staticPlacement(conRect, parentRect, rootElement);
+  }
 
-    // this.useAutoPlacement = this.detectIfOutsideScreen(conRect, parentRect);
-    // if (this.useAutoPlacement) {
-    //   this.autoPlacement(conRect, parentRect, rootElement);
-    // } else {
-    //   this.staticPlacement(conRect, parentRect, rootElement);
-    // }
+  staticPlacement(
+    conRect: DOMRect,
+    parentRect: DOMRect,
+    rootElement: HTMLElement
+  ) {
+    if (parentRect != null && conRect != null && rootElement != null) {
+      rootElement.style.top = '';
+      rootElement.style.bottom = '';
+      rootElement.style.left = '';
+      rootElement.style.right = '';
+
+      let originX = 0;
+      let originY = 0;
+      let alignX = 0;
+      let alignY = 0;
+
+      let marginX = 0;
+      let marginY = 0;
+
+      // -------- TOP / BOT --------
+      if (this.overlayPosCurrent.indexOf('top') !== -1) {
+        alignY = 1;
+        originY = 0;
+        marginY = this.margin;
+      }
+
+      if (this.overlayPosCurrent.indexOf('bot') !== -1) {
+        alignY = 0;
+        originY = 1;
+        marginY = this.margin;
+      }
+
+      if (
+        this.overlayPosCurrent.indexOf('top') !== -1 ||
+        this.overlayPosCurrent.indexOf('bot') !== -1
+      ) {
+        if (this.overlayPosCurrent.indexOf('Center') !== -1) {
+          alignX = 0.5;
+          originX = 0.5;
+        }
+      }
+
+      if (this.overlayPosCurrent.indexOf('Left') !== -1) {
+        alignX = 0;
+        originX = 0;
+      }
+      if (this.overlayPosCurrent.indexOf('Right') !== -1) {
+        alignX = 1;
+        originX = 1;
+      }
+
+      // -------- LEFT / RIGHT --------
+      if (this.overlayPosCurrent.indexOf('left') !== -1) {
+        alignX = 1;
+        originX = 0;
+        marginX = this.margin;
+      }
+      if (this.overlayPosCurrent.indexOf('right') !== -1) {
+        alignX = 0;
+        originX = 1;
+        marginX = this.margin;
+      }
+
+      if (
+        this.overlayPosCurrent.indexOf('left') !== -1 ||
+        this.overlayPosCurrent.indexOf('right') !== -1
+      ) {
+        if (this.overlayPosCurrent.indexOf('Center') !== -1) {
+          alignY = 0.5;
+          originY = 0.5;
+        }
+      }
+
+      if (this.overlayPosCurrent.indexOf('Top') !== -1) {
+        alignY = 0;
+        originY = 0;
+      }
+      if (this.overlayPosCurrent.indexOf('Bot') !== -1) {
+        alignY = 1;
+        originY = 1;
+      }
+
+      const calcX =
+        -conRect.width * alignX +
+        parentRect.width * originX -
+        marginX * (alignX * 2 - 1);
+      const calcY =
+        -conRect.height * alignY -
+        parentRect.height * (1 - originY) -
+        marginY * (alignY * 2 - 1);
+
+      rootElement.style.left = `${calcX}px`;
+      rootElement.style.top = `${calcY}px`;
+    }
+  }
+
+  render() {
+    return this.open
+      ? html`
+          <div id="container">
+            <slot></slot>
+          </div>
+        `
+      : '';
+  }
+
+  // ------ BELOW IS NOT IN USE A THE MOMENT ------
+
+  @property({ type: Boolean }) useAutoPlacement = false;
+
+  mathClamp(value: number, min: number, max: number) {
+    if (value < min) {
+      return min;
+    } else if (value > max) {
+      return max;
+    }
+    return value;
+  }
+
+  autoPlacement(
+    conRect: DOMRect,
+    parentRect: DOMRect,
+    rootElement: HTMLElement
+  ) {
+    if (this.open) {
+      // For Convience and readability
+      const halfWindowX = window.innerWidth / 2;
+      const halfWindowY = window.innerHeight / 2;
+      const halfConX = conRect.width / 2;
+      const halfConY = conRect.height / 2;
+      const halfParentX = parentRect.width / 2;
+      const halfParentY = parentRect.height / 2;
+
+      const centerToParentX = halfWindowX - parentRect.x;
+      const centerToParentY = halfWindowY - parentRect.y;
+
+      // Is the center of the screen within the X or Y axis of the component?
+      const isInCenterX =
+        centerToParentX > 0 - halfConX &&
+        centerToParentX < parentRect.width + halfConX;
+      const isInCenterY =
+        centerToParentY > 0 - halfConY &&
+        centerToParentY < parentRect.height + halfConY;
+
+      // Detect which side of the center the parent element is on, on both axes.
+      const posXFlip = parentRect.x + halfParentX - halfWindowX < 0;
+      const posYFlip = parentRect.y + halfParentY - halfWindowY < 0;
+
+      // Flip the target left/right and top/down based on the position of the parent element relative to the screen center
+      const posXInput = posXFlip ? window.innerWidth : 0;
+      const posYInput = posYFlip ? window.innerHeight : 0;
+
+      // Clamp the overlay container to the edges of the parent element.
+      const posX = this.mathClamp(
+        posXInput,
+        parentRect.x - conRect.width,
+        parentRect.x + parentRect.width
+      );
+      const posY = this.mathClamp(
+        posYInput,
+        parentRect.y - conRect.height,
+        parentRect.y + parentRect.height
+      );
+
+      // Clamp the overlay to the screen so that it "always" stays inside:
+      const posXClamp = this.mathClamp(
+        posX,
+        0,
+        window.innerWidth - conRect.width
+      );
+      const posYClamp = this.mathClamp(
+        posY,
+        0,
+        window.innerHeight - conRect.height
+      );
+
+      // Make the overlay container glide along the edges of the parent.
+      const posXFinal = isInCenterX
+        ? centerToParentX + parentRect.x - halfConX
+        : posXClamp;
+      const posYFinal = isInCenterY
+        ? centerToParentY + parentRect.y - halfConY
+        : posYClamp;
+
+      // Make sure the overlay container doesn't overlap the parent and that it stays on screen.
+      const posYActualFinal = isInCenterX
+        ? this.mathClamp(
+            centerToParentY - halfParentY < 0
+              ? parentRect.y - conRect.height
+              : parentRect.y + parentRect.height,
+            0,
+            window.innerHeight - conRect.height
+          )
+        : posYFinal;
+
+      // Apply the positions as styling
+      rootElement.style.top = `${posYActualFinal}px`;
+      rootElement.style.left = `${posXFinal}px`;
+    }
   }
 
   detectIfOutsideScreen(conRect: DOMRect, parentRect: DOMRect): boolean {
@@ -313,289 +491,5 @@ export class UUIOverlayElement extends LitElement {
       isOutsideScreen = outRight || outLeft || outTop || outBot;
     }
     return isOutsideScreen;
-  }
-
-  convertPosToVector() {
-    return { originX: 0, originY: 0 };
-  }
-
-  staticPlacement(
-    conRect: DOMRect,
-    parentRect: DOMRect,
-    rootElement: HTMLElement
-  ) {
-    if (parentRect != null && conRect != null && rootElement != null) {
-      rootElement.style.top = '';
-      rootElement.style.bottom = '';
-      rootElement.style.left = '';
-      rootElement.style.right = '';
-
-      let originX = 0;
-      let originY = 0;
-      let alignX = 0;
-      let alignY = 0;
-
-      let marginX = 0;
-      let marginY = 0;
-
-      if (this.overlayPosCurrent.indexOf('top') !== -1) {
-        alignY = 1;
-        originY = 0;
-        marginY = this.margin;
-      }
-
-      if (this.overlayPosCurrent.indexOf('bot') !== -1) {
-        alignY = 0;
-        originY = 1;
-        marginY = this.margin;
-      }
-
-      if (
-        this.overlayPosCurrent.indexOf('top') !== -1 ||
-        this.overlayPosCurrent.indexOf('bot') !== -1
-      ) {
-        if (this.overlayPosCurrent.indexOf('Center') !== -1) {
-          alignX = 0.5;
-          originX = 0.5;
-        }
-      }
-
-      if (this.overlayPosCurrent.indexOf('Left') !== -1) {
-        alignX = 0;
-        originX = 0;
-      }
-      if (this.overlayPosCurrent.indexOf('Right') !== -1) {
-        alignX = 1;
-        originX = 1;
-      }
-
-      if (this.overlayPosCurrent.indexOf('left') !== -1) {
-        alignX = 1;
-        originX = 0;
-        marginX = this.margin;
-      }
-      if (this.overlayPosCurrent.indexOf('right') !== -1) {
-        alignX = 0;
-        originX = 1;
-        marginX = this.margin;
-      }
-
-      if (
-        this.overlayPosCurrent.indexOf('left') !== -1 ||
-        this.overlayPosCurrent.indexOf('right') !== -1
-      ) {
-        if (this.overlayPosCurrent.indexOf('Center') !== -1) {
-          alignY = 0.5;
-          originY = 0.5;
-        }
-      }
-
-      if (this.overlayPosCurrent.indexOf('Top') !== -1) {
-        alignY = 0;
-        originY = 0;
-      }
-      if (this.overlayPosCurrent.indexOf('Bot') !== -1) {
-        alignY = 1;
-        originY = 1;
-      }
-
-      const calcX =
-        -conRect.width * alignX +
-        parentRect.width * originX -
-        marginX * (alignX * 2 - 1);
-      const calcY =
-        -conRect.height * alignY -
-        parentRect.height * (1 - originY) -
-        marginY * (alignY * 2 - 1);
-
-      rootElement.style.left = `${calcX}px`;
-      rootElement.style.top = `${calcY}px`;
-
-      // if (
-      //   this.overlayPosCurrent === 'topLeft' ||
-      //   this.overlayPosCurrent === 'topCenter' ||
-      //   this.overlayPosCurrent === 'topRight'
-      // ) {
-
-      //   //rootElement.style.top = `${-conRect.height - parentRect.height}px`;
-      //   switch (this.overlayPosCurrent) {
-      //     case 'topLeft':
-      //       rootElement.style.left = `${0}px`;
-      //       break;
-      //     case 'topCenter':
-      //       rootElement.style.left = `${
-      //          (parentRect.width / 2 - conRect.width / 2)
-      //       }px`;
-      //       break;
-      //     case 'topRight':
-      //       rootElement.style.left = `${(parentRect.width - conRect.width)}px`;
-      //       // conElement.style.left = `${Number.parseFloat(conElement.style.left.split('px')[0]) ? Number.parseFloat(conElement.style.left.split('px')[0]) : 0 + this.mathClamp(-conRect.x, 0, Infinity)}px`;
-      //       // console.log(conElement, Number.parseFloat(conElement.style.left.split('px')[0]))
-      //       break;
-      //   }
-      // }
-
-      // if (
-      //   this.overlayPosCurrent === 'botLeft' ||
-      //   this.overlayPosCurrent === 'botCenter' ||
-      //   this.overlayPosCurrent === 'botRight'
-      // ) {
-      //   //rootElement.style.top = `${0}px`;
-      //   switch (this.overlayPosCurrent) {
-      //     case 'botLeft':
-      //       rootElement.style.left = `${0}px`;
-      //       break;
-      //     case 'botCenter':
-      //       rootElement.style.left = `${
-      //          + (parentRect.width / 2 - conRect.width / 2)
-      //       }px`;
-      //       break;
-      //     case 'botRight':
-      //       rootElement.style.left = `${
-      //          + parentRect.width - conRect.width
-      //       }px`;
-      //       break;
-      //   }
-      // }
-
-      // if (
-      //   this.overlayPosCurrent === 'leftTop' ||
-      //   this.overlayPosCurrent === 'leftCenter' ||
-      //   this.overlayPosCurrent === 'leftBot'
-      // ) {
-      //   rootElement.style.left = `${ -conRect.width}px`;
-      //   switch (this.overlayPosCurrent) {
-      //     case 'leftTop':
-      //       rootElement.style.top = `${-parentRect.height}px`;
-      //       break;
-      //     case 'leftCenter':
-      //       rootElement.style.top = `${-parentRect.height / 2 - conRect.height / 2}px`;
-      //       break;
-      //     case 'leftBot':
-      //       rootElement.style.top = `${-conRect.height}px`;
-      //       break;
-      //   }
-      // }
-
-      // if (
-      //   this.overlayPosCurrent === 'rightTop' ||
-      //   this.overlayPosCurrent === 'rightCenter' ||
-      //   this.overlayPosCurrent === 'rightBot'
-      // ) {
-      //   rootElement.style.left = `${ + parentRect.width}px`;
-      //   switch (this.overlayPosCurrent) {
-      //     case 'rightTop':
-      //       rootElement.style.top = `${-parentRect.height}px`;
-      //       break;
-      //     case 'rightCenter':
-      //       rootElement.style.top = `${-parentRect.height / 2 - conRect.height / 2}px`;
-      //       break;
-      //     case 'rightBot':
-      //       rootElement.style.top = `${-conRect.height}px`;
-      //       break;
-      //   }
-      // }
-    }
-  }
-
-  autoPlacement(
-    conRect: DOMRect,
-    parentRect: DOMRect,
-    rootElement: HTMLElement
-  ) {
-    if (this.open) {
-      // For Convience and readability
-      const halfWindowX = window.innerWidth / 2;
-      const halfWindowY = window.innerHeight / 2;
-      const halfConX = conRect.width / 2;
-      const halfConY = conRect.height / 2;
-      const halfParentX = parentRect.width / 2;
-      const halfParentY = parentRect.height / 2;
-
-      const centerToParentX = halfWindowX - parentRect.x;
-      const centerToParentY = halfWindowY - parentRect.y;
-
-      // Is the center of the screen within the X or Y axis of the component?
-      const isInCenterX =
-        centerToParentX > 0 - halfConX &&
-        centerToParentX < parentRect.width + halfConX;
-      const isInCenterY =
-        centerToParentY > 0 - halfConY &&
-        centerToParentY < parentRect.height + halfConY;
-
-      // Detect which side of the center the parent element is on, on both axes.
-      const posXFlip = parentRect.x + halfParentX - halfWindowX < 0;
-      const posYFlip = parentRect.y + halfParentY - halfWindowY < 0;
-
-      // Flip the target left/right and top/down based on the position of the parent element relative to the screen center
-      const posXInput = posXFlip ? window.innerWidth : 0;
-      const posYInput = posYFlip ? window.innerHeight : 0;
-
-      // Clamp the overlay container to the edges of the parent element.
-      const posX = this.mathClamp(
-        posXInput,
-        parentRect.x - conRect.width,
-        parentRect.x + parentRect.width
-      );
-      const posY = this.mathClamp(
-        posYInput,
-        parentRect.y - conRect.height,
-        parentRect.y + parentRect.height
-      );
-
-      // Clamp the overlay to the screen so that it "always" stays inside:
-      const posXClamp = this.mathClamp(
-        posX,
-        0,
-        window.innerWidth - conRect.width
-      );
-      const posYClamp = this.mathClamp(
-        posY,
-        0,
-        window.innerHeight - conRect.height
-      );
-
-      // Make the overlay container glide along the edges of the parent.
-      const posXFinal = isInCenterX
-        ? centerToParentX + parentRect.x - halfConX
-        : posXClamp;
-      const posYFinal = isInCenterY
-        ? centerToParentY + parentRect.y - halfConY
-        : posYClamp;
-
-      // Make sure the overlay container doesn't overlap the parent and that it stays on screen.
-      const posYActualFinal = isInCenterX
-        ? this.mathClamp(
-            centerToParentY - halfParentY < 0
-              ? parentRect.y - conRect.height
-              : parentRect.y + parentRect.height,
-            0,
-            window.innerHeight - conRect.height
-          )
-        : posYFinal;
-
-      // Apply the positions as styling
-      rootElement.style.top = `${posYActualFinal}px`;
-      rootElement.style.left = `${posXFinal}px`;
-    }
-  }
-
-  mathClamp(value: number, min: number, max: number) {
-    if (value < min) {
-      return min;
-    } else if (value > max) {
-      return max;
-    }
-    return value;
-  }
-
-  render() {
-    return this.open
-      ? html`
-          <div id="container">
-            <slot></slot>
-          </div>
-        `
-      : '';
   }
 }
