2d42b08c18ed68ee0ba6d22fcc4f344563f7b3c6
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2021-08-23 14:32:52 +0200

    tooltip component now handles open/close

--- src/components/uui-tooltip/uui-tooltip.element.ts
+++ src/components/uui-tooltip/uui-tooltip.element.ts
@@ -1,6 +1,7 @@
 import { property, state, query } from 'lit/decorators';
 import { LitElement, html, css } from 'lit';
 import { OverlayPosition } from '../uui-overlay/uui-overlay.element';
+import { UUIOverlayEvent } from '../uui-overlay/UUIOverlayEvent';
 
 /**
  *  @element uui-tooltip
@@ -15,8 +16,12 @@ export class UUITooltipElement extends LitElement {
   ];
 
   @property({ type: String }) position: OverlayPosition = 'topRight';
-  @property({ type: Boolean })
-  open = false;
+  @property({ type: Boolean, attribute: 'use-click' }) useClick = false;
+  @property({ type: Boolean }) open = false;
+
+  openTooltip(value: boolean) {
+    this.open = value;
+  }
 
   render() {
     return html`
@@ -25,8 +30,15 @@ export class UUITooltipElement extends LitElement {
           .margin=${10}
           .open=${this.open}
           .overlayPos=${this.position}
+          @change=${(e: UUIOverlayEvent) => (this.open = e.target.open)}
         >
-          <slot name="parent" slot="parent"></slot>
+          <slot
+            name="parent"
+            slot="parent"
+            @click=${() => (this.useClick ? this.openTooltip(!this.open) : '')}
+            @mouseover=${() => (this.useClick ? '' : this.openTooltip(true))}
+            @mouseout=${() => (this.useClick ? '' : this.openTooltip(false))}
+          ></slot>
           <slot name="overlay" slot="overlay"></slot>
         </uui-overlay>
       </div>
--- src/components/uui-tooltip/uui-tooltip.story.ts
+++ src/components/uui-tooltip/uui-tooltip.story.ts
@@ -1,6 +1,4 @@
-import { LitElement } from 'lit';
 import { html } from 'lit-html';
-import { customElement, state } from 'lit/decorators';
 import '.';
 
 export default {
@@ -8,28 +6,40 @@ export default {
   component: 'uui-tooltip',
 };
 
-function open(e: any, open: boolean) {
-  e.target.parentElement.open = open;
-}
+const renderButton = () => {
+  return html` <uui-button look="primary" label="Open" slot="parent">
+  </uui-button>`;
+};
+
+const renderIcon = () => {
+  return html`
+    <div
+      slot="parent"
+      style="width: 32px; height: 32px; border-radius: 100%; border: 2px solid #3544b1; display: flex; align-items: center; justify-content: center;"
+    >
+      <uui-icon
+        name="info"
+        style="color: #3544b1; pointer-events: none"
+      ></uui-icon>
+    </div>
+  `;
+};
+
+const renderContent = () => {
+  return html` <div
+    slot="overlay"
+    style="border: 1px solid black; border-radius: 10px; padding: 12px; width: 300px; box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.05);"
+  >
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta
+    nisi id nunc sagittis, eget tincidunt libero posuere. Integer
+  </div>`;
+};
 
 export const Click = () => {
   const element = html`
     <div style="margin: 200px; padding: 200px;">
-      <uui-tooltip position="topRight">
-        <uui-button
-          look="primary"
-          label="Open"
-          @click=${(e: any) => open(e, !e.target.parentElement.open)}
-          slot="parent"
-        />
-        </uui-button>
-        <div
-          slot="overlay"
-          style="border: 1px solid black; border-radius: 10px; padding: 12px; width: 300px; box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.05);"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-          porta nisi id nunc sagittis, eget tincidunt libero posuere. Integer
-        </div>
+      <uui-tooltip position="topRight" use-click>
+        ${renderButton()} ${renderContent()}
       </uui-tooltip>
     </div>
   `;
@@ -41,24 +51,7 @@ export const Hover = () => {
   const element = html`
     <div style="margin: 200px; padding: 200px;">
       <uui-tooltip position="rightTop">
-        <div
-          @mouseover=${(e: any) => open(e, true)}
-          @mouseout=${(e: any) => open(e, false)}
-          slot="parent"
-          style="width: 32px; height: 32px; border-radius: 100%; border: 2px solid #3544b1; display: flex; align-items: center; justify-content: center;"
-        >
-          <uui-icon
-            name="info"
-            style="color: #3544b1; pointer-events: none"
-          ></uui-icon>
-        </div>
-        <div
-          slot="overlay"
-          style="border: 1px solid black; border-radius: 10px; padding: 12px; width: 300px; box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.05);"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-          porta nisi id nunc sagittis, eget tincidunt libero posuere. Integer
-        </div>
+        ${renderIcon()} ${renderContent()}
       </uui-tooltip>
     </div>
   `;
