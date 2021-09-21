6baa3bd69fe127a3111eb17172ab7ca835d755c5
Julia gru <julia.gru@gmail.com>
2021-09-21 15:57:41 +0200

    insert fallback values to css

--- tasks/rollup-plugin-fallback.js
+++ tasks/rollup-plugin-fallback.js
@@ -4,9 +4,10 @@ import { walk } from 'estree-walker';
 import postcss from 'postcss';
 import * as fs from 'fs/promises';
 import path from 'path';
-import { parse, stringify } from 'postcss-values-parser';
+import * as postCssValueParser from 'postcss-values-parser';
 
 import postcssCustomProperties from 'postcss-custom-properties';
+import postcssCustomPropertiesFallback from 'postcss-custom-properties-fallback';
 // import{ plugin as postCssAdvancedVariables} from 'postcss-advanced-variables'
 // import * as colorFuncvions from 'postcss-color-function'
 
@@ -24,15 +25,6 @@ export default function addFallbackValues(options = {}) {
 
       const cssFile = await fs.readFile(CSS_PATH, 'utf8');
 
-      // await postcssCustomProperties.process(
-      //   cssFile,
-      //   { from: CSS_PATH, to: './css-test.css' },
-      //   {
-      //     importFrom: [CSS_PATH],
-      //     exportTo: customProperties,
-      //   } /*, processOptions, pluginOptions */
-      // );
-
       const cssResult = await postcss([
         postcssCustomProperties({
           importFrom: [CSS_PATH],
@@ -41,22 +33,19 @@ export default function addFallbackValues(options = {}) {
       ]).process(cssFile, { from: CSS_PATH, to: './css-test.css' });
 
       for (const key in customProperties.customProperties) {
-        const valueNode = parse(customProperties.customProperties[key]);
-        const onlyVars = valueNode.nodes.filter(node => node.isVar)
+        const valueNode = postCssValueParser.parse(
+          customProperties.customProperties[key]
+        );
+        const onlyVars = valueNode.nodes.filter(node => node.isVar);
         if (onlyVars.length === 1) {
-           
-          const keyToFind = onlyVars[0].params.trim().substring(1, onlyVars[0].params.length-1);
+          const keyToFind = onlyVars[0].params
+            .trim()
+            .substring(1, onlyVars[0].params.length - 1);
 
-          customProperties.customProperties[key] = customProperties.customProperties[keyToFind];
-
-          //console.log(key, customProperties.customProperties[key])
-
+          customProperties.customProperties[key] =
+            customProperties.customProperties[keyToFind];
         }
-        //if (valueNode.nodes[0].isVar) console.log(key, valueNode);
       }
-
-
-
 
       return customProperties;
     } catch (err) {
@@ -69,17 +58,8 @@ export default function addFallbackValues(options = {}) {
     async buildStart() {
       console.log('buildStart');
       properties = await extractCustomProperties();
-    console.log(properties);
-
-     
-    //   //console.log(properties);
-     },
+    },
     async transform(code) {
-      // console.log(
-      //   'your custom properties',
-      //   properties.customProperties['--uui-color-cocoa-brown-dark']
-      // );
-
       const ast = this.parse(code);
 
       walk(ast, {
@@ -89,13 +69,14 @@ export default function addFallbackValues(options = {}) {
             parent.type === 'ArrayExpression'
           ) {
             if (node.tag.name === 'css') {
-              // /.replace('\n', '')
-              //console.log('NODE',node, 'PARENT',parent, 'PROP', prop, 'INDEX', index)
-              let css = node.quasi.quasis[0].value.raw;
-              //console.log(node.quasi.quasis[0].value.raw)
-              postcss.parse(css);
+              let cssString = node.quasi.quasis[0].value.raw;
+
+              postcss([
+                postcssCustomPropertiesFallback({ importFrom: properties }),
+              ])
+                .process(cssString)
+                .then(result => console.log(result.css));
             }
-            // console.log(css)
           }
         },
       });
